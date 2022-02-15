5e77e105fc6b599251fad2dd0628b0e1fd92afe2
Evan Doyle <evanmdoyle@gmail.com>
2022-02-14 16:15:43 -0800

    Implement basic creation instruction for ExpenseManager PDA

--- programs/riptide-q1/src/lib.rs
+++ programs/riptide-q1/src/lib.rs
@@ -10,175 +10,35 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod riptide_q1 {
     use super::*;
-    pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> ProgramResult {
-        let game = &mut ctx.accounts.game;
-        game.players = [ctx.accounts.player_one.key(), player_two];
-        game.turn = 1;
+    pub fn create_expense_manager(
+        ctx: Context<CreateExpenseManager>,
+        name: String,
+        _bump: u8,
+    ) -> ProgramResult {
+        let expense_manager = &mut ctx.accounts.expense_manager;
+        // TODO: check too large? or is that automatic?
+        expense_manager.name = name;
         Ok(())
     }
-    pub fn play(ctx: Context<Play>, tile: Tile) -> ProgramResult {
-        let game = &mut ctx.accounts.game;
-
-        require!(
-            game.current_player() == ctx.accounts.player.key(),
-            TicTacToeError::NotPlayersTurn
-        );
-
-        game.play(&tile)
-    }
-}
-
-#[derive(Accounts)]
-pub struct SetupGame<'info> {
-    #[account(init, payer = player_one, space = Game::MAXIMUM_SIZE + 8)]
-    pub game: Account<'info, Game>,
-    #[account(mut)]
-    pub player_one: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Play<'info> {
-    #[account(mut)]
-    pub game: Account<'info, Game>,
-    pub player: Signer<'info>,
 }
 
 #[account]
 #[derive(Default)]
-pub struct Game {
-    players: [Pubkey; 2],
-    turn: u8,
-    board: [[Option<Sign>; 3]; 3],
-    state: GameState,
+pub struct ExpenseManager {
+    pub name: String,
+    pub treasury_account: Option<Pubkey>, // TODO: what is the best way to connect treasury?
 }
 
-impl Game {
-    const MAXIMUM_SIZE: usize = 116;
-
-    pub fn is_active(&self) -> bool {
-        self.state == GameState::Active
-    }
-
-    fn current_player_index(&self) -> usize {
-        ((self.turn - 1) % 2) as usize
-    }
-
-    pub fn current_player(&self) -> Pubkey {
-        self.players[self.current_player_index()]
-    }
-
-    pub fn play(&mut self, tile: &Tile) -> ProgramResult {
-        if !self.is_active() {
-            return Err(TicTacToeError::GameAlreadyOver.into());
-        }
-        match tile {
-            tile
-            @ Tile {
-                row: 0..=2,
-                column: 0..=2,
-            } => match self.board[tile.row as usize][tile.column as usize] {
-                Some(_) => return Err(TicTacToeError::TileAlreadySet.into()),
-                None => {
-                    self.board[tile.row as usize][tile.column as usize] =
-                        Some(Sign::from_usize(self.current_player_index()).unwrap());
-                }
-            },
-            _ => return Err(TicTacToeError::TileOutOfBounds.into()),
-        }
-
-        self.update_state();
-
-        if let GameState::Active = self.state {
-            self.turn += 1;
-        }
-
-        Ok(())
-    }
-
-    fn is_winning_trio(&self, trio: [(usize, usize); 3]) -> bool {
-        let [first, second, third] = trio;
-        self.board[first.0][first.1].is_some()
-            && self.board[first.0][first.1] == self.board[second.0][second.1]
-            && self.board[first.0][first.1] == self.board[third.0][third.1]
-    }
-
-    fn update_state(&mut self) {
-        for i in 0..=2 {
-            // three of the same in one row
-            if self.is_winning_trio([(i, 0), (i, 1), (i, 2)]) {
-                self.state = GameState::Won {
-                    winner: self.current_player(),
-                };
-                return;
-            }
-            // three of the same in one column
-            if self.is_winning_trio([(0, i), (1, i), (2, i)]) {
-                self.state = GameState::Won {
-                    winner: self.current_player(),
-                };
-                return;
-            }
-        }
-
-        // three of the same in one diagonal
-        if self.is_winning_trio([(0, 0), (1, 1), (2, 2)])
-            || self.is_winning_trio([(0, 2), (1, 1), (2, 0)])
-        {
-            self.state = GameState::Won {
-                winner: self.current_player(),
-            };
-            return;
-        }
-
-        // reaching this code means the game has not been won,
-        // so if there are unfilled tiles left, it's still active
-        for row in 0..=2 {
-            for column in 0..=2 {
-                if self.board[row][column].is_none() {
-                    return;
-                }
-            }
-        }
-
-        // game has not been won
-        // game has no more free tiles
-        // -> game ends in a tie
-        self.state = GameState::Tie;
-    }
+impl ExpenseManager {
+    const MAX_SIZE: usize = 96;
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize)]
-pub struct Tile {
-    row: u8,
-    column: u8,
-}
-
-#[error]
-pub enum TicTacToeError {
-    TileOutOfBounds,
-    TileAlreadySet,
-    GameAlreadyOver,
-    NotPlayersTurn,
-}
-
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
-pub enum GameState {
-    Active,
-    Tie,
-    Won { winner: Pubkey },
-}
-
-impl Default for GameState {
-    fn default() -> Self {
-        Self::Active
-    }
-}
-
-#[derive(
-    AnchorSerialize, AnchorDeserialize, FromPrimitive, ToPrimitive, Copy, Clone, PartialEq, Eq,
-)]
-pub enum Sign {
-    X,
-    O,
+#[derive(Accounts)]
+#[instruction(name: String, bump: u8)]
+pub struct CreateExpenseManager<'info> {
+    #[account(init, seeds = [b"expense_manager", name.as_bytes()], bump = bump, payer = dao_member, space = ExpenseManager::MAX_SIZE + 8)]
+    pub expense_manager: Account<'info, ExpenseManager>,
+    #[account(mut)]
+    pub dao_member: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
--- tests/riptide-q1.ts
+++ tests/riptide-q1.ts
@@ -1,129 +1,27 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { RiptideQ1 } from "../target/types/riptide_q1";
-import { assert, expect } from "chai";
+import { expect } from "chai";
 
-async function setup(program) {
-  const gameKeypair = anchor.web3.Keypair.generate();
-  const playerOne = program.provider.wallet; // from Anchor.toml, this is also the payer so wallet needs funds
-  const playerTwo = anchor.web3.Keypair.generate();
-  await program.rpc.setupGame(playerTwo.publicKey, {
+async function setup(program: Program<RiptideQ1>, name: string) {
+  const daoMember = program.provider.wallet; // from Anchor.toml, this is also the payer so wallet needs funds
+  const [expenseManagerPDA, bump] =
+    anchor.utils.publicKey.findProgramAddressSync(
+      [Buffer.from("expense_manager"), Buffer.from(name)],
+      program.programId
+    );
+  await program.rpc.createExpenseManager(name, bump, {
     accounts: {
-      game: gameKeypair.publicKey,
-      playerOne: playerOne.publicKey,
+      expenseManager: expenseManagerPDA,
+      daoMember: daoMember.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
     },
-    signers: [gameKeypair], // playerOne automatically included as a signer because it is listed as the provider
+    signers: [], // playerOne automatically included as a signer because it is listed as the provider
   });
   return {
-    gameKeypair,
-    playerOne,
-    playerTwo,
+    daoMember,
+    expenseManagerPDA,
   };
-}
-
-async function createSetupInstruction(program) {
-  const gameKeypair = anchor.web3.Keypair.generate();
-  const playerOne = program.provider.wallet; // from Anchor.toml, this is also the payer so wallet needs funds
-  const playerTwo = anchor.web3.Keypair.generate();
-  const instruction = await program.instruction.setupGame(playerTwo.publicKey, {
-    accounts: {
-      game: gameKeypair.publicKey,
-      playerOne: playerOne.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    },
-  });
-  return {
-    instruction,
-    gameKeypair,
-    playerOne,
-    playerTwo,
-  };
-}
-
-async function playFirstMove(
-  program,
-  tile,
-  expectedTurn,
-  expectedGameState,
-  expectedBoard
-) {
-  const {
-    instruction: setupInstruction,
-    gameKeypair,
-    playerOne,
-  } = await createSetupInstruction(program);
-  await program.rpc.play(tile, {
-    preInstructions: [setupInstruction],
-    accounts: {
-      player: playerOne.publicKey,
-      game: gameKeypair.publicKey,
-    },
-    signers: [gameKeypair], // playerOne automatically included as a signer because it is listed as the provider
-  });
-
-  const gameState = await program.account.game.fetch(gameKeypair.publicKey);
-  expect(gameState.turn).to.equal(expectedTurn);
-  expect(gameState.state).to.eql(expectedGameState);
-  expect(gameState.board).to.eql(expectedBoard);
-}
-
-async function play(
-  program,
-  game,
-  player,
-  tile,
-  expectedTurn,
-  expectedGameState,
-  expectedBoard
-) {
-  await program.rpc.play(tile, {
-    accounts: {
-      player: player.publicKey,
-      game,
-    },
-    signers: player instanceof (anchor.Wallet as any) ? [] : [player], // don't pass second copy of provider wallet
-  });
-
-  const gameState = await program.account.game.fetch(game);
-  expect(gameState.turn).to.equal(expectedTurn);
-  expect(gameState.state).to.eql(expectedGameState);
-  expect(gameState.board).to.eql(expectedBoard);
-}
-
-async function createPlayInstruction(program, game, player, tile) {
-  return await program.instruction.play(tile, {
-    accounts: {
-      player: player.publicKey,
-      game,
-    },
-  });
-}
-
-async function setupAndPlayAsPostInstructions(
-  program,
-  gameKeypair,
-  playerOne,
-  playerTwo,
-  instructions,
-  expectedTurn,
-  expectedGameState,
-  expectedBoard
-) {
-  await program.rpc.setupGame(playerTwo.publicKey, {
-    accounts: {
-      game: gameKeypair.publicKey,
-      playerOne: playerOne.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    },
-    postInstructions: instructions,
-    signers: [gameKeypair, playerTwo],
-  });
-
-  const gameState = await program.account.game.fetch(gameKeypair.publicKey);
-  expect(gameState.turn).to.equal(expectedTurn);
-  expect(gameState.state).to.eql(expectedGameState);
-  expect(gameState.board).to.eql(expectedBoard);
 }
 
 describe("riptide-q1", () => {
@@ -132,161 +30,11 @@ describe("riptide-q1", () => {
 
   const program = anchor.workspace.RiptideQ1 as Program<RiptideQ1>;
 
-  it("setup game sets state correctly", async () => {
-    const { gameKeypair, playerOne, playerTwo } = await setup(program);
-    let gameState = await program.account.game.fetch(gameKeypair.publicKey);
-    expect(gameState.turn).to.equal(1);
-    expect(gameState.players).to.eql([
-      playerOne.publicKey,
-      playerTwo.publicKey,
-    ]);
-    expect(gameState.state).to.eql({ active: {} });
-    expect(gameState.board).to.eql([
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ]);
-  });
-
-  it("should setup game as pre-instruction and play one move", async () => {
-    await playFirstMove(program, { row: 2, column: 2 }, 2, { active: {} }, [
-      [null, null, null],
-      [null, null, null],
-      [null, null, { x: {} }],
-    ]);
-  });
-
-  it("player one wins", async () => {
-    const { gameKeypair, playerOne, playerTwo } = await setup(program);
-    await play(
-      program,
-      gameKeypair.publicKey,
-      playerOne,
-      { row: 0, column: 0 },
-      2,
-      { active: {} },
-      [
-        [{ x: {} }, null, null],
-        [null, null, null],
-        [null, null, null],
-      ]
+  it("creates expense manager", async () => {
+    const { expenseManagerPDA } = await setup(program, "testing manager");
+    let expenseManagerData = await program.account.expenseManager.fetch(
+      expenseManagerPDA
     );
-    await play(
-      program,
-      gameKeypair.publicKey,
-      playerTwo,
-      { row: 0, column: 1 },
-      3,
-      { active: {} },
-      [
-        [{ x: {} }, { o: {} }, null],
-        [null, null, null],
-        [null, null, null],
-      ]
-    );
-    await play(
-      program,
-      gameKeypair.publicKey,
-      playerOne,
-      { row: 1, column: 0 },
-      4,
-      { active: {} },
-      [
-        [{ x: {} }, { o: {} }, null],
-        [{ x: {} }, null, null],
-        [null, null, null],
-      ]
-    );
-    await play(
-      program,
-      gameKeypair.publicKey,
-      playerTwo,
-      { row: 0, column: 2 },
-      5,
-      { active: {} },
-      [
-        [{ x: {} }, { o: {} }, { o: {} }],
-        [{ x: {} }, null, null],
-        [null, null, null],
-      ]
-    );
-    await play(
-      program,
-      gameKeypair.publicKey,
-      playerOne,
-      { row: 2, column: 0 },
-      5,
-      { won: { winner: playerOne.publicKey } },
-      [
-        [{ x: {} }, { o: {} }, { o: {} }],
-        [{ x: {} }, null, null],
-        [{ x: {} }, null, null],
-      ]
-    );
-  });
-
-  it("should play entirely as post-instructions", async () => {
-    const gameKeypair = anchor.web3.Keypair.generate();
-    const playerOne = program.provider.wallet; // from Anchor.toml, this is also the payer so wallet needs funds
-    const playerTwo = anchor.web3.Keypair.generate();
-    const instructions = [
-      await createPlayInstruction(program, gameKeypair.publicKey, playerOne, {
-        row: 0,
-        column: 0,
-      }),
-      await createPlayInstruction(program, gameKeypair.publicKey, playerTwo, {
-        row: 0,
-        column: 1,
-      }),
-      await createPlayInstruction(program, gameKeypair.publicKey, playerOne, {
-        row: 1,
-        column: 0,
-      }),
-      await createPlayInstruction(program, gameKeypair.publicKey, playerTwo, {
-        row: 0,
-        column: 2,
-      }),
-      await createPlayInstruction(program, gameKeypair.publicKey, playerOne, {
-        row: 2,
-        column: 0,
-      }),
-    ];
-    await setupAndPlayAsPostInstructions(
-      program,
-      gameKeypair,
-      playerOne,
-      playerTwo,
-      instructions,
-      5,
-      { won: { winner: playerOne.publicKey } },
-      [
-        [{ x: {} }, { o: {} }, { o: {} }],
-        [{ x: {} }, null, null],
-        [{ x: {} }, null, null],
-      ]
-    );
-  });
-
-  it("should throw error for tile out of bounds", async () => {
-    const { gameKeypair, playerOne } = await setup(program);
-    try {
-      await play(
-        program,
-        gameKeypair.publicKey,
-        playerOne,
-        { row: 5, column: 1 }, // out of bounds row
-        4,
-        { active: {} },
-        [
-          [null, null, null],
-          [null, null, null],
-          [null, null, null],
-        ]
-      );
-      // we use this to make sure we definitely throw an error
-      assert(false, "should've failed but didn't ");
-    } catch (error) {
-      expect(error.code).to.equal(6000);
-    }
+    expect(expenseManagerData.name).to.equal("testing manager");
   });
 });
