49f76ee5923813a2798096f24b46c8055584e24c
Evan Doyle <evanmdoyle@gmail.com>
2022-02-15 11:51:19 -0800

    Assign txn signer as authority on expense manager oncreate

--- programs/riptide-q1/src/lib.rs
+++ programs/riptide-q1/src/lib.rs
@@ -16,8 +16,10 @@ pub mod riptide_q1 {
         _bump: u8,
     ) -> ProgramResult {
         let expense_manager = &mut ctx.accounts.expense_manager;
-        // TODO: check too large? or is that automatic?
+        let authority = &ctx.accounts.authority;
+        // TODO: check if name is too large and send meaningful error code
         expense_manager.name = name;
+        expense_manager.authority = authority.key();
         Ok(())
     }
 }
@@ -26,19 +28,58 @@ pub mod riptide_q1 {
 #[derive(Default)]
 pub struct ExpenseManager {
     pub name: String,
-    pub treasury_account: Option<Pubkey>, // TODO: what is the best way to connect treasury?
+    pub authority: Pubkey,
 }
 
 impl ExpenseManager {
+    // name: 64
+    // authority: 32
     const MAX_SIZE: usize = 96;
+}
+
+#[account]
+#[derive(Default)]
+pub struct ExpensePackage {
+    pub name: String,
+    pub description: String,
+    pub state: ExpensePackageState,
+}
+
+impl ExpensePackage {
+    const MAX_SIZE: usize = 321;
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum ExpensePackageState {
+    CREATED,
+    PENDING,
+    DENIED,
+    ACCEPTED,
+    PAID,
+}
+
+impl Default for ExpensePackageState {
+    fn default() -> Self {
+        Self::CREATED
+    }
+}
+
+#[account]
+#[derive(Default)]
+pub struct TransactionExpense {
+    pub signature: String,
+}
+
+impl TransactionExpense {
+    const MAX_SIZE: usize = 100;
 }
 
 #[derive(Accounts)]
 #[instruction(name: String, bump: u8)]
 pub struct CreateExpenseManager<'info> {
-    #[account(init, seeds = [b"expense_manager", name.as_bytes()], bump = bump, payer = dao_member, space = ExpenseManager::MAX_SIZE + 8)]
+    #[account(init, seeds = [b"expense_manager", name.as_bytes()], bump = bump, payer = authority, space = ExpenseManager::MAX_SIZE + 8)]
     pub expense_manager: Account<'info, ExpenseManager>,
     #[account(mut)]
-    pub dao_member: Signer<'info>,
+    pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
--- tests/riptide-q1.ts
+++ tests/riptide-q1.ts
@@ -1,10 +1,10 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { RiptideQ1 } from "../target/types/riptide_q1";
-import { expect } from "chai";
+import { assert, expect } from "chai";
 
 async function setup(program: Program<RiptideQ1>, name: string) {
-  const daoMember = program.provider.wallet; // from Anchor.toml, this is also the payer so wallet needs funds
+  const authority = program.provider.wallet;
   const [expenseManagerPDA, bump] =
     anchor.utils.publicKey.findProgramAddressSync(
       [Buffer.from("expense_manager"), Buffer.from(name)],
@@ -13,28 +13,31 @@ async function setup(program: Program<RiptideQ1>, name: string) {
   await program.rpc.createExpenseManager(name, bump, {
     accounts: {
       expenseManager: expenseManagerPDA,
-      daoMember: daoMember.publicKey,
+      authority: authority.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
     },
-    signers: [], // playerOne automatically included as a signer because it is listed as the provider
+    signers: [],
   });
   return {
-    daoMember,
+    authority,
     expenseManagerPDA,
   };
 }
 
 describe("riptide-q1", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.RiptideQ1 as Program<RiptideQ1>;
 
-  it("creates expense manager", async () => {
-    const { expenseManagerPDA } = await setup(program, "testing manager");
+  it("creates expense manager with correct initial values", async () => {
+    const { authority, expenseManagerPDA } = await setup(
+      program,
+      "testing manager"
+    );
     let expenseManagerData = await program.account.expenseManager.fetch(
       expenseManagerPDA
     );
     expect(expenseManagerData.name).to.equal("testing manager");
+    assert(expenseManagerData.authority.equals(authority.publicKey));
   });
 });
