45d25d97dffa6aee6cec6700d4b4c4a944d89621
Evan Doyle <evanmdoyle@gmail.com>
2022-02-15 17:46:45 -0800

    Rename riptide-q1 -> slide; implement AddTransactionHash instruction

--- programs/slide/src/lib.rs
+++ programs/slide/src/lib.rs
@@ -8,7 +8,7 @@ use num_traits::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod riptide_q1 {
+pub mod slide {
     use super::*;
     pub fn create_expense_manager(
         ctx: Context<CreateExpenseManager>,
@@ -33,13 +33,27 @@ pub mod riptide_q1 {
         // TODO: how to authorize a user to create an expense package?
         // TODO: check expense package starts empty here
         let expense_package = &mut ctx.accounts.expense_package;
-        let user = &ctx.accounts.user;
+        let owner = &ctx.accounts.owner;
         // TODO: check string field lengths
         expense_package.name = name;
         expense_package.description = description;
-        expense_package.owner = user.key();
+        expense_package.owner = owner.key();
         // TODO: check expense manager is initialized
         expense_package.expense_manager = expense_manager_address;
+        Ok(())
+    }
+    // TODO: this probably needs to accept a Vec of hashes
+    pub fn add_transaction_hash(
+        ctx: Context<AddTransactionHash>,
+        transaction_hash: String,
+        _expense_manager_address: Pubkey,
+        _bump: u8,
+    ) -> ProgramResult {
+        // TODO: check expense manager in correct state
+        let expense_package = &mut ctx.accounts.expense_package;
+        // TODO: check hash length
+        // TODO: check vec length
+        expense_package.transaction_hashes.push(transaction_hash);
         Ok(())
     }
 }
@@ -62,18 +76,22 @@ impl ExpenseManager {
 pub struct ExpensePackage {
     pub name: String,
     pub description: String,
+    pub transaction_hashes: Vec<String>,
     pub owner: Pubkey,
     pub expense_manager: Pubkey,
     pub state: ExpensePackageState,
 }
 
+// TODO: once rent-refunds are implemented (receipts)
+//   might be able to stop storing the hashes on-chain?
 impl ExpensePackage {
     // name: 64
     // description: 256
+    // transaction_hashes: 32 * 32 = 1024
     // owner: 32
     // expense_manager: 32
     // state: 1
-    const MAX_SIZE: usize = 385;
+    const MAX_SIZE: usize = 1409;
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
@@ -91,22 +109,11 @@ impl Default for ExpensePackageState {
     }
 }
 
-#[account]
-#[derive(Default)]
-pub struct TransactionExpense {
-    pub signature: String,
-}
-
-impl TransactionExpense {
-    const MAX_SIZE: usize = 100;
-}
-
 #[derive(Accounts)]
 #[instruction(name: String, bump: u8)]
 pub struct CreateExpenseManager<'info> {
     #[account(init, seeds = [b"expense_manager", name.as_bytes()], bump = bump, payer = authority, space = ExpenseManager::MAX_SIZE + 8)]
     pub expense_manager: Account<'info, ExpenseManager>,
-    #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
@@ -114,9 +121,16 @@ pub struct CreateExpenseManager<'info> {
 #[derive(Accounts)]
 #[instruction(name: String, description: String, expense_manager_address: Pubkey, bump: u8)]
 pub struct CreateExpensePackage<'info> {
-    #[account(init, seeds = [b"expense_package", expense_manager_address.as_ref(), user.key().as_ref()], bump = bump, payer = user, space = ExpensePackage::MAX_SIZE + 8)]
+    #[account(init, seeds = [b"expense_package", expense_manager_address.as_ref(), owner.key().as_ref()], bump = bump, payer = owner, space = ExpensePackage::MAX_SIZE + 8)]
     pub expense_package: Account<'info, ExpensePackage>,
-    #[account(mut)]
-    pub user: Signer<'info>,
+    pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(transaction_hash: String, expense_manager_address: Pubkey, bump: u8)]
+pub struct AddTransactionHash<'info> {
+    #[account(mut, seeds = [b"expense_package", expense_manager_address.as_ref(), owner.key().as_ref()], bump = bump, has_one = owner)]
+    pub expense_package: Account<'info, ExpensePackage>,
+    pub owner: Signer<'info>,
 }
---
+++ tests/slide.ts
@@ -0,0 +1,176 @@
+import { PublicKey } from "@solana/web3.js";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Slide } from "../target/types/slide";
+import { assert, expect } from "chai";
+
+function getExpenseManagerAddressAndBump(
+  name: string,
+  programId: PublicKey
+): [PublicKey, number] {
+  return anchor.utils.publicKey.findProgramAddressSync(
+    [Buffer.from("expense_manager"), Buffer.from(name)],
+    programId
+  );
+}
+
+function getExpensePackageAddress(
+  expenseManagerPDA: anchor.web3.PublicKey,
+  owner: anchor.web3.PublicKey,
+  programId: PublicKey
+): [PublicKey, number] {
+  return anchor.utils.publicKey.findProgramAddressSync(
+    [
+      Buffer.from("expense_package"),
+      expenseManagerPDA.toBuffer(),
+      owner.toBuffer(),
+    ],
+    programId
+  );
+}
+
+async function setupExpenseManager(program: Program<Slide>, name: string) {
+  const authority = program.provider.wallet;
+  const [expenseManagerPDA, bump] = getExpenseManagerAddressAndBump(
+    name,
+    program.programId
+  );
+  await program.rpc.createExpenseManager(name, bump, {
+    accounts: {
+      expenseManager: expenseManagerPDA,
+      authority: authority.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    },
+    signers: [],
+  });
+  return {
+    authority,
+    expenseManagerPDA,
+  };
+}
+
+async function setupExpensePackage(
+  program: Program<Slide>,
+  name: string,
+  description: string,
+  user: anchor.web3.PublicKey,
+  expenseManagerPDA: anchor.web3.PublicKey
+) {
+  const [expensePackagePDA, bump] = getExpensePackageAddress(
+    expenseManagerPDA,
+    user,
+    program.programId
+  );
+  await program.rpc.createExpensePackage(
+    name,
+    description,
+    expenseManagerPDA,
+    bump,
+    {
+      accounts: {
+        expensePackage: expensePackagePDA,
+        owner: user,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [],
+    }
+  );
+  return {
+    expensePackagePDA,
+  };
+}
+
+async function addTransactionHash(
+  program: Program<Slide>,
+  transactionHash: string,
+  user: anchor.web3.PublicKey,
+  expenseManagerPDA: anchor.web3.PublicKey
+) {
+  const [expensePackagePDA, bump] = getExpensePackageAddress(
+    expenseManagerPDA,
+    user,
+    program.programId
+  );
+  await program.rpc.addTransactionHash(
+    transactionHash,
+    expenseManagerPDA,
+    bump,
+    {
+      accounts: {
+        expensePackage: expensePackagePDA,
+        owner: user,
+      },
+    }
+  );
+  return {
+    expensePackagePDA,
+  };
+}
+
+describe("slide", () => {
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.Slide as Program<Slide>;
+
+  it("creates expense manager with correct initial values", async () => {
+    const { authority, expenseManagerPDA } = await setupExpenseManager(
+      program,
+      "testing manager"
+    );
+    let expenseManagerData = await program.account.expenseManager.fetch(
+      expenseManagerPDA
+    );
+    expect(expenseManagerData.name).to.equal("testing manager");
+    assert(expenseManagerData.authority.equals(authority.publicKey));
+  });
+
+  it("creates an expense package with correct initial values", async () => {
+    const { authority, expenseManagerPDA } = await setupExpenseManager(
+      program,
+      "testing manager 2"
+    );
+    const { expensePackagePDA } = await setupExpensePackage(
+      program,
+      "myexpense",
+      "I bought some stuff on ebay",
+      authority.publicKey,
+      expenseManagerPDA
+    );
+    let expensePackageData = await program.account.expensePackage.fetch(
+      expensePackagePDA
+    );
+    expect(expensePackageData.name).to.equal("myexpense");
+    expect(expensePackageData.description).to.equal(
+      "I bought some stuff on ebay"
+    );
+    assert(expensePackageData.owner.equals(authority.publicKey));
+    assert(expensePackageData.expenseManager.equals(expenseManagerPDA));
+    expect(expensePackageData.state).to.eql({ created: {} });
+  });
+
+  it("adds a transaction hash to expense package", async () => {
+    const { authority, expenseManagerPDA } = await setupExpenseManager(
+      program,
+      "testing manager 3"
+    );
+    await setupExpensePackage(
+      program,
+      "myexpense",
+      "I bought some stuff on ebay",
+      authority.publicKey,
+      expenseManagerPDA
+    );
+    const { expensePackagePDA } = await addTransactionHash(
+      program,
+      "faketransactionhash",
+      authority.publicKey,
+      expenseManagerPDA
+    );
+    let expensePackageData = await program.account.expensePackage.fetch(
+      expensePackagePDA
+    );
+    expect(expensePackageData.transactionHashes).to.eql([
+      "faketransactionhash",
+    ]);
+  });
+});
